e1acc9239f4f299adc4b473a82fe7cbcecc99d5d
Claudianus Mamertus <claudianus@engineer.com>
2019-02-11 10:48:33 +0900

    @

--- app/Controllers/Http/PageController.js
+++ app/Controllers/Http/PageController.js
@@ -1,36 +1,37 @@
-'use strict'
+"use strict";
 
-const Url = use('App/Models/Url')
-const base62 = require('base62/lib/ascii')
+const Url = use("App/Models/Url");
+const base62 = require("base62/lib/ascii");
 
 class PageController {
-  async home({
-    view,
-    request,
-    response
-  }) {
+  async home({ view, request, response }) {
     //url db rows 담을 변수
-    let urlRows
+    let urlRows;
 
     //클라이언트가 보낸 urls 쿠키 값 가져오기
-    const cookie = request.plainCookie('urls')
+    const cookie = request.plainCookie("urls");
     if (cookie) {
       //값이 있으면
       //파싱해서 배열로 만듬
-      let urlIds = cookie.split('|')
+      let urlIds = cookie.split("|");
       //empty 항목 제거
-      urlIds = urlIds.filter(Boolean)
+      urlIds = urlIds.filter(Boolean);
       //62진수 id들 10진수로 변환
-      urlIds = urlIds.map((x) => base62.decode(x))
+      urlIds = urlIds.map(x => base62.decode(x));
       //10진수id로 db에서 url row들 가져옴
-      urlRows = await Url.query().whereIn('id', urlIds).orderBy('id', 'desc').limit(20).fetch()
+      urlRows = await Url.query()
+        .whereIn("id", urlIds)
+        .orderBy("id", "desc")
+        .limit(20)
+        .fetch();
+      urlRows = urlRows.toJSON();
     }
 
-    response.header('cache-control', 'no-store')
-    return view.render('home', {
-      urlRows: urlRows.toJSON()
-    })
+    response.header("cache-control", "no-store");
+    return view.render("home", {
+      urlRows
+    });
   }
 }
 
-module.exports = PageController
+module.exports = PageController;
